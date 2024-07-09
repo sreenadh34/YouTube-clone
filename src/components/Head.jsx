c27f9ea8df1202e67e15533c456b426c79b1123f
sreenadh34 <43.sreenadh@gmail.com>
2024-07-10 00:44:02 +0530

    search suggestions

--- src/components/Head.jsx
+++ src/components/Head.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from "react-redux";
 import { toggleMenu } from '../utils/appSlice'
+import { YOUTUBE_SEARCH_API } from '../utils/contants';
 
 const Head = () => {
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const [suggestions, setSuggestions] = useState([]);
+    const [showSuggestions, setShowSuggestions] = useState(false);
+      
+  useEffect(() => {
+   const timer = setTimeout(() => getSearchSugsestions(),200);
+   return () => {
+    clearTimeout(timer);
+   }
+  },[searchQuery]);
+    
+  const getSearchSugsestions = async () =>{
+    const data =await fetch(YOUTUBE_SEARCH_API+ searchQuery);
+    const json =await data.json();
+    setSuggestions(json[1]);
+  }
+
   const dispatch = useDispatch()
 const toggleMenuHandler = () => {
   dispatch(toggleMenu());
@@ -25,10 +44,27 @@ const toggleMenuHandler = () => {
          </a>
       </div>
       <div className='col-span-10 px-10'>
-        <input className="w-1/2 border border-gray-400 p-2 rounded-l-full" type="text" />
+        <div>
+        <input 
+           className=" px-5 w-1/2 border border-gray-400 p-2 rounded-l-full"
+           type="text"
+           value={searchQuery}
+           onChange={(e) => setSearchQuery(e.target.value)}
+           onFocus={()=>setShowSuggestions(true)}
+           onBlur={()=>setShowSuggestions(false)}
+           />
         <button className="border border-gray-400 px-5 p-2 rounded-r-full bg-gray-100">
-          🔍
+        🔍
         </button>
+        </div>
+        {showSuggestions && (
+          <div className='fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border-gray-100'>
+          <ul>
+            {suggestions.map((s) => (<li key={s} className='py-2 px-3 shadow-sm hover:bg-gray-100'>🔍{s}</li>
+           ) )}
+          </ul>
+        </div>
+        )}
       </div>
       <div className='col-span-1'>
         <img 
