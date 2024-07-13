68c7a28ce9f7af313c199938e87dcd6198198c47
sreenadh34 <43.sreenadh@gmail.com>
2024-07-14 00:08:22 +0530

    random live chat

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage }from "../utils/chatSlice";
 import ChatMessage from './ChatMessage';
+import { generateRandomName, makeRandomMessage } from '../utils/helper';
 
 const LiveChat = () => {
 
@@ -14,21 +15,30 @@ const LiveChat = () => {
         //API Polling
         console.log("API polling");
         dispatch(addMessage({
-          name:"Akshay Saini",
-          message:"Lorem Ipsum Dolor Site Amet",
+          name:generateRandomName(),
+          message:makeRandomMessage(20),
         }))
-    },2000);
+    },500);
     return () => clearInterval(i);
   },[]);
 
   return (
-    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll">
-
+    <>
+    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+    <div>
     { // Dont use indexes as key
       chatMessages.map((c, i) => (<ChatMessage key={i} name={c.name} message={c.message} />
         ))
     }
     </div>
+    </div>
+
+    <div className='w-full p-2 ml-2 border border-black'>
+      <input className='w-[275px]' type="text" />
+      <button className='px-2 mx-2 bg-green-100'>Send</button>
+    </div>
+   
+    </>
   );
 };
 
